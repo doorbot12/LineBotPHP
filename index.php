212b62c4b416187ae75140c98d968c2cd01b4cd0
Farkhan Azmi <fliazmi@gmail.com>
2017-11-09 20:51:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -54,6 +54,28 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
     }
  
     // kode aplikasi nanti disini
+    $data = json_decode($body, true);
+    if(is_array($data['events'])){
+        foreach ($data['events'] as $event)
+        {
+            if ($event['type'] == 'message')
+            {
+                if($event['message']['type'] == 'text')
+                {
+                    // send same message as reply to user
+                    $result = $bot->replyText($event['replyToken'], $event['message']['text']);
+     
+                    // or we can use replyMessage() instead to send reply message
+                    // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
+                    // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+     
+                    return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
+                }
+            }
+        }
+    }
+
+    $bot->replyText($replyToken, 'ini pesan balasan');
  
 });
  
