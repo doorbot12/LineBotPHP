3c1f1bcf1cdfe0b6a40bbb808d58a49766362fea
Farkhan Azmi <fliazmi@gmail.com>
2018-01-10 14:22:50 +0700

    gg

--- index.php
+++ index.php
@@ -88,12 +88,13 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                         if (strcasecmp($event['message']['text'],'token')==0) {
                             $result = $bot->replyText($event['replyToken'], $event['replyToken']);
                         }
-                        $gg=substr("$event['message']['text']",3);
-                        $sc = new Scrape('p'+$gg,substr($gg,7));
+                        $gg ="p" . substr($event['message']['text'],3);
+                        $bb= substr($gg ,8);
+                        $sc = new Scrape($gg , $bb);
                         $hasil = $sc->login();
-                        $result = $bot->replyText($event['replyToken'], $event['message']['text']);
+                        //$result = $bot->replyText($event['replyToken'], $event['message']['text']);
                         
-                        //$result = $bot->replyText($event['replyToken'], $hasil);
+                        $result = $bot->replyText($event['replyToken'], $hasil);
          
                         // or we can use replyMessage() instead to send reply message
                         // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
