fecd95cc1a7557a47f829af2d05c4eba934ff9dc
Farkhan Azmi <fliazmi@gmail.com>
2018-01-11 05:12:43 +0700

    v

--- index.php
+++ index.php
@@ -75,33 +75,38 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
 
                         //$result = $bot->replyText($event['replyToken'], $userId);    
                         if (substr($event['message']['text'],0,2)=='IP' & strlen($event['message']['text'])==18){
-                            $gg ="p" . substr($event['message']['text'],3);
-                            $bb= substr($gg ,8);
-                            $sc = new Scrape($gg , $bb);
-                            $hasil = $sc->login();
-                            if ($hasil!='Transit') {
-                                # code...
-                                $result = $bot->replyText($event['replyToken'], $event['message']['text'] . $hasil);
+
+                            if ($userId == 'U4f3b524bfcd08556173108d04ae067ad') {
+                                $gg ="p" . substr($event['message']['text'],3);
+                                $bb= substr($gg ,8);
+                                $sc = new Scrape($gg , $bb);
+                                $hasil = $sc->login();
+                                if ($hasil!='Transit') {
+                                    $result = $bot->replyText($event['replyToken'], $event['message']['text'] . $hasil);
+                                }else{
+                                    $result = $bot->replyText($event['replyToken'], $event['message']['text'] .'Tidak Dapat Diakses');
+                                }
                             }else{
-                                $result = $bot->replyText($event['replyToken'], $event['message']['text'] .'Tidak Dapat Diakses');
+                                $result = $bot->replyText($event['replyToken'],'Maaf '.$profile['displayName'].' Bot dalam perbaikan')
                             }
-                            //$result = $bot->replyText($event['replyToken'], $event['message']['text']);
+
+
                             
+                        }else if (substr($event['message']['text'],0,3)=='IPK' & strlen($event['message']['text'])==19){
                             
-                        }
-                        else if (substr($event['message']['text'],0,3)=='IPK' & strlen($event['message']['text'])==19){
-                            $gg ="p" . substr($event['message']['text'],4);
-                            $bb= substr($gg ,8);
-                            $sc = new Scrape($gg , $bb);
-                            $hasil = $sc->login2();
-                            if ($hasil!='Transit') {
-                                # code...
-                                $result = $bot->replyText($event['replyToken'], $event['message']['text'] . $hasil);
+                            if ($userId == 'U4f3b524bfcd08556173108d04ae067ad') {
+                                $gg ="p" . substr($event['message']['text'],4);
+                                $bb= substr($gg ,8);
+                                $sc = new Scrape($gg , $bb);
+                                $hasil = $sc->login2();
+                                if ($hasil!='Transit') {
+                                    $result = $bot->replyText($event['replyToken'], $event['message']['text'] . $hasil);
+                                }else{
+                                    $result = $bot->replyText($event['replyToken'], $event['message']['text'] .'Tidak Dapat Diakses');
+                                }
                             }else{
-                                $result = $bot->replyText($event['replyToken'], $event['message']['text'] .'Tidak Dapat Diakses');
+                                $result = $bot->replyText($event['replyToken'],'Maaf '.$profile['displayName'].' Bot dalam perbaikan')
                             }
-                            //$result = $bot->replyText($event['replyToken'], $event['message']['text']);
-                            
                             
                         }
                         // if (strpos($event['message']['text'], 'ip') !== false) {
@@ -145,22 +150,42 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                             $getprofile = $bot->getProfile($userId);
                             $profile    = $getprofile->getJSONDecodedBody();
                             $greetings  = new TextMessageBuilder("Halo, ".$profile['displayName']);
-                            //$result = $bot->replyText($event['replyToken'], );
                             $result = $bot->replyText($event['replyToken'], $userId);
                         }
+
                         if (substr($event['message']['text'],0,2)=='IP' & strlen($event['message']['text'])==18){
-                            $gg ="p" . substr($event['message']['text'],3);
-                            $bb= substr($gg ,8);
-                            $sc = new Scrape($gg , $bb);
-                            $hasil = $sc->login();
-                            if ($hasil!='Transit') {
-                                # code...
-                                $result = $bot->replyText($event['replyToken'], $event['message']['text'] . $hasil);
+
+                            if ($userId == 'U4f3b524bfcd08556173108d04ae067ad') {
+                                $gg ="p" . substr($event['message']['text'],3);
+                                $bb= substr($gg ,8);
+                                $sc = new Scrape($gg , $bb);
+                                $hasil = $sc->login();
+                                if ($hasil!='Transit') {
+                                    $result = $bot->replyText($event['replyToken'], $event['message']['text'] . $hasil);
+                                }else{
+                                    $result = $bot->replyText($event['replyToken'], $event['message']['text'] .'Tidak Dapat Diakses');
+                                }
                             }else{
-                                $result = $bot->replyText($event['replyToken'], $event['message']['text'] .'Tidak Dapat Diakses');
+                                $result = $bot->replyText($event['replyToken'],'Maaf '.$profile['displayName'].' Bot dalam perbaikan')
                             }
-                            //$result = $bot->replyText($event['replyToken'], $event['message']['text']);
+
+
                             
+                        }else if (substr($event['message']['text'],0,3)=='IPK' & strlen($event['message']['text'])==19){
+                            
+                            if ($userId == 'U4f3b524bfcd08556173108d04ae067ad') {
+                                $gg ="p" . substr($event['message']['text'],4);
+                                $bb= substr($gg ,8);
+                                $sc = new Scrape($gg , $bb);
+                                $hasil = $sc->login2();
+                                if ($hasil!='Transit') {
+                                    $result = $bot->replyText($event['replyToken'], $event['message']['text'] . $hasil);
+                                }else{
+                                    $result = $bot->replyText($event['replyToken'], $event['message']['text'] .'Tidak Dapat Diakses');
+                                }
+                            }else{
+                                $result = $bot->replyText($event['replyToken'],'Maaf '.$profile['displayName'].' Bot dalam perbaikan')
+                            }
                             
                         }
                         // or we can use replyMessage() instead to send reply message
