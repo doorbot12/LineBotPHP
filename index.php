1f7e6c31bda9faa73e28988e33466598c799db54
Farkhan Azmi <fliazmi@gmail.com>
2018-03-04 22:20:43 +0700

    asasa

--- index.php
+++ index.php
@@ -130,7 +130,7 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                                     echo $datanyas['jadwal'];
                                     foreach($datanyas as $datanyass)
                                     {
-                                        $hasilnya=$hasilnya.$datanyass['nama_jadwal'] . "\n";
+                                        $hasilnya=$hasilnya."\n".$datanyass['nama_jadwal'];
                                     }
                                 }   
                             }
@@ -145,7 +145,7 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                                     echo $datanyas['jadwal'];
                                     foreach($datanyas as $datanyass)
                                     {
-                                        $hasilnya=$hasilnya.$datanyass['detail'] . "\n";
+                                        $hasilnya=$hasilnya."\n".$datanyass['detail'];
                                     }
                                 }   
                             }
