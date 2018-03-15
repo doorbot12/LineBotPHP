f3880b7b866746e8ba740c1016c52a25cac516d7
Farkhan Azmi <fliazmi@gmail.com>
2018-03-16 03:21:13 +0700

    hn

--- index.php
+++ index.php
@@ -128,7 +128,7 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                                 $multiMessageBuilder = new MultiMessageBuilder();
                                 if ($obj['graphql']['user']['is_private']!="false") {
                                     $nomer=0;
-                                    if ((!empty($a[2])) && ($$a[2]<=12) {
+                                    if ((!empty($a[2])) && ($$a[2]<=12)) {
                                         $nomer=$a[2]-1;
                                     }
                                     $linkfoto=$obj['graphql']['user']['edge_owner_to_timeline_media']['edges']["$nomer"]['node']['display_url'];
@@ -212,7 +212,7 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                                 $multiMessageBuilder = new MultiMessageBuilder();
                                 if ($obj['graphql']['user']['is_private']!="false") {
                                     $nomer=0;
-                                    if ((!empty($a[2])) && ($$a[2]<=12) {
+                                    if ((!empty($a[2])) && ($$a[2]<=12)) {
                                         $nomer=$a[2]-1;
                                     }
                                     $linkfoto=$obj['graphql']['user']['edge_owner_to_timeline_media']['edges']["$nomer"]['node']['display_url'];
