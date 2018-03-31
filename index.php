bbb31f66b2f9b24d8a3a6d94a182ff3a263ab54a
Farkhan Azmi <fliazmi@gmail.com>
2018-03-31 23:13:36 +0700

    sss

--- index.php
+++ index.php
@@ -293,7 +293,7 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                         if ($userId=="U4f3b524bfcd08556173108d04ae067ad") {
                             
                             if ($a[0]=="/phprun") {
-                                //$babi=file_get_contents('http://farkhan.000webhostapp.com/nutshell/babi.php?php='.urldecode($a[1]));
+                                $babi=file_get_contents('http://farkhan.000webhostapp.com/nutshell/babi.php?php='.urldecode($a[1]));
                                 $babi2=file_get_contents('http://farkhan.000webhostapp.com/nutshell/data.php');
                                 $result = $bot->replyText($event['replyToken'], htmlspecialchars($babi2));
                             }
